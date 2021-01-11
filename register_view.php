b7fae160bd24ec63848cc675f8ca17b51c02eb6b
JoreyLensink <joreylensink01@gmail.com>
2021-01-11 13:44:42 +0100

    .

---
+++ register_view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>register</title>
+</head>
+
+<body>
+    <div class="container">
+        <h2>Registeren</h2>
+        <form method="POST" action="register.php">
+            <input name="firstname" class="form-input" type="text" plaeholder="Voornaam">
+            <input name="lastname" class="form-input" type="text" plaeholder="Achternaam">
+            <input name="email" class="form-input" type="text" plaeholder="Email">
+            <input name="password" class="form-input" type="password" plaeholder="Wachtwoord">
+
+            <button name="submit" type="submit">Registreren</button>
+            </input>
+
+        </form>
+    </div>
+</body>
+
+</html>
