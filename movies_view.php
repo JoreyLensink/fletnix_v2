a98e5a769eaf5754b073d27742fa7cf0ce0b8a11
Tiesi2810 <74417394+Tiesi2810@users.noreply.github.com>
2021-01-11 16:19:19 +0100

    titel werkt soort van

---
+++ movies_view.php
@@ -0,0 +1,64 @@
+<?php
+include 'controller/movies.php';
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="nl">
+
+<head>
+    <meta charset=utf-8>
+    <title>Films</title>
+    <link rel="icon" href="assets/images/logo-browser.png">
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+
+<body>
+   <!-- <div id="background"> -->
+    <!-- Navigatie balk -->
+    <!-- <div id="navbar">
+        <a class="navbar-logo" href="index.html"><img class="logo-nav" src="assets/images/logo.png" alt="logo fletnix"></a>
+        <div class="dropdown">
+            <a href="movies.html"><div class="dropbtn">Films</div></a>
+            <div  class="dropdown-content">
+                <a href="movies.html#animation">Animatie</a>
+                <a href="movies.html#adventure">Avontuur</a>
+                <a href="movies.html#humour">Humor</a>
+            </div>
+        </div>
+        <a class="navbar-link" href="about.html">Over ons</a>
+        <a class="navbar-link" href="contact.html">Contact</a>
+        <a class="navbar-link" href="subscription.html">Abonnementen</a>
+        <a class="navbar-link-right" href="account.html"><img class="login-icon" src="assets/images/icon-login.png"
+                                                              alt="icon person"></a>
+    </div> -->
+
+    <div class="main">
+        <div class="movie-category">
+        <?php
+            echo "<h1> </h1>";
+             ?>
+            <div class="movie-item">
+                <img alt="movie" class="othermovies" src="assets/images/COCO.jpg">
+                <h2 class="movie-hover-item">
+                <?php
+               echo '<span>' . ValueMovie($dbh) . '</span>' ;
+                 ?>
+                    <br/>
+                    <span>Animatie / Kinderen</span>
+                    <br/>
+                    <a href="watch.html">
+                        <span>Film kijken</span>
+                    </a>
+                </h2>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+</body>
+</html>
+
