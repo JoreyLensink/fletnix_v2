16cb720085399983253caa8a50581820551f651b
Tiesi2810 <74417394+Tiesi2810@users.noreply.github.com>
2021-01-14 12:44:57 +0100

    execute

--- controller/movies.php
+++ controller/movies.php
@@ -3,13 +3,14 @@ require_once "connection.php";
 
 // //begin code voor films ophalen
 function valueMovie($dbh, $genre, $aantal){
-$movies      = $dbh->query("SELECT top ($aantal)   Movie.title, Movie.price, Movie.movie_id 
+$movies      = $dbh->prepare("SELECT top ($aantal)   Movie.title, Movie.price, Movie.movie_id 
 FROM Movie
 LEFT JOIN Movie_Genre ON Movie.movie_id = Movie_Genre.movie_id
 LEFT JOIN Movie_Cast ON Movie.movie_id = Movie_Cast.movie_id
 LEFT JOIN Movie_Director ON Movie.movie_id = Movie_Director.movie_id
 WHERE Movie_Genre.genre_name = '$genre' AND Movie_Cast.person_id IS NOT NULL AND Movie_Director.person_id IS NOT NULL
 GROUP BY Movie.movie_id, Movie.title, Movie.price ");
+$movies->execute();         
 return $movies;
 }
      
--- controller/watch.php
+++ controller/watch.php
@@ -7,31 +7,25 @@ require_once "movies.php";
 
 function loadDirectorsInfo($dbh, $movieID) {
 
-$directorsInfo      = $dbh->query(" SELECT * 
+$directorsInfo      = $dbh->prepare(" SELECT * 
 FROM PERSON
 LEFT JOIN Movie_Director ON Person.person_id = Movie_Director.person_id
 LEFT JOIN Movie ON Movie_Director.movie_id = Movie.movie_id
 WHERE Movie.movie_id = $movieID AND Movie_Director.person_id IS NOT NULL");
+$directorsInfo->execute();
 return $directorsInfo;
 }
 
 function loadCastInfo($dbh, $movieID) {
 
-    $CastInfo      = $dbh->query(" SELECT * , Movie.duration, Movie.publication_year, Movie.description
+    $CastInfo      = $dbh->prepare(" SELECT * , Movie.duration, Movie.publication_year, Movie.description
     FROM PERSON
     LEFT JOIN Movie_Cast ON Person.person_id = Movie_Cast.person_id
     LEFT JOIN Movie ON Movie_Cast.movie_id = Movie.movie_id
     WHERE Movie.movie_id = $movieID AND Movie_Cast.person_id IS NOT NULL ");
+    $CastInfo->execute();
     return $CastInfo;
     }
 
 
 print_r($_POST);
-
-
-
-
-
-
-
-?>
